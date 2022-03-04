491b447befe1a445d8619f6e3eb3638aff387934
chenwenfeng <w712915@gmail.com>
2022-03-04 22:32:45 +0900

    ダミーjsonの読み込みの微調整

--- src/App.js
+++ src/App.js
@@ -1,20 +1,25 @@
 import React, { useState, useEffect } from 'react';
 import RankingList from './components/RankingList';
+import Data from './data.json';
 
 function App() {
   const [posts, setPosts] = useState([])
-  const GET_URL = 'https://webcdn.17app.co/campaign/pretest/data.json'
+  // const GET_URL = 'https://webcdn.17app.co/campaign/pretest/data.json'
 
   useEffect(() => {
-    fetch(GET_URL, {method: 'GET'})
-    .then(res => res.json())
-      .then(data => {
-        const damiPosts = data
-        for (let i = 0; i < data.length; i++) {
-            damiPosts[i].rank = i
-        }
-        setPosts(damiPosts)
-    })
+    // fetch(GET_URL, {method: 'GET'})
+    // .then(res => res.json())
+    //   .then(data => {
+    //     const damiPosts = data
+    //     for (let i = 0; i < data.length; i++) {
+    //         damiPosts[i].rank = i
+    //     }
+    //     setPosts(damiPosts)
+    //   })
+    for (let i = 0; i < Data.length; i++) {
+        Data[i].rank = i
+    }
+    setPosts(Data)
   }, [])
 
   if (posts.length === 0) {
